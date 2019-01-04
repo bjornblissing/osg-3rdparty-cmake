abcf182b8a2a747b7b01743ba629ad128d4ebcc3
Björn Blissing <bjorn.blissing@vti.se>
2019-01-04 22:42:51 +0100

    Use AppVeyors cache functionallity
    By caching downloaded dependencies we speed up repetitive builds.

---
+++ download_dependencies.bat
@@ -0,0 +1,19 @@
+set HOME_DIR=%cd%
+echo %HOME_DIR%
+git clone --depth 1 --single-branch --branch v1.2.11 https://github.com/madler/zlib.git c:/3rdparty/zlib
+git clone --depth 1 --single-branch --branch v1.6.34  git://git.code.sf.net/p/libpng/code.git c:/3rdparty/libpng-1.6.34
+curl -O http://www.ijg.org/files/jpegsr9c.zip
+7z x jpegsr9c.zip -oc:\3rdparty\
+git clone --depth 1 --single-branch --branch Release-v4-0-9 https://github.com/vadz/libtiff.git c:/3rdparty/tiff-4.0.9
+git clone --depth 1 --single-branch --branch VER-2-9-1 git://git.sv.nongnu.org/freetype/freetype2.git c:/3rdparty/freetype-2.9.1
+git clone https://github.com/markkilgard/glut.git c:/3rdparty/glut
+cd c:\3rdparty\glut
+git reset --hard 8cd96cb440f1f2fac3a154227937be39d06efa53
+cd %HOME_DIR%
+git clone --depth 1 --single-branch --branch 5.1.4 https://git.code.sf.net/p/giflib/code.git c:/3rdparty/giflib-5.1.4
+git clone https://github.com/nmoinvaz/minizip.git c:/3rdparty/minizip
+cd c:/3rdparty/minizip
+git reset --hard dac37702b3fab4068ac9a7c4a992df7f0e4f14df
+cd %HOME_DIR%
+curl -O https://curl.haxx.se/download/curl-7.60.0.zip
+7z x curl-7.60.0.zip -oc:\3rdparty\
