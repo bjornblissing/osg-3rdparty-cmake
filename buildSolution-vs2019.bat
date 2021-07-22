f59796d7c5e2248fc920284d099cc5741fb18cdc
szh <szh@test.com>
2021-07-22 16:13:03 +0800

    modify for linux

--- buildSolution-vs2019.bat
+++ buildSolution-vs2019.bat
@@ -1,12 +1,25 @@
-@echo on
+@echo off
 cd /d %~dp0
+REM ROOT
+set ThirdPary_ROOT=%1
+REM x64 x86
+set arch=%2
+
+if "%ThirdPary_ROOT%"=="" (
+	set ThirdPary_ROOT=.\..\..\
+)
+
+if "%arch%"=="" (
+	set arch=x64
+)
+
+echo %arch%
 set ROOT=%~dp0
 set SRC=%ROOT%\3rdparty\src
-set name=v142-x64
-md build
-md build\osg-3rdparty
-md 3rdparty-bin
-md 3rdparty-bin\v142-x64
+
+set build_dir=%ThirdPary_ROOT%build\osg-3rdparty
+
+md %build_dir%
 
 set curl=%SRC%/curl
 set freetype=%SRC%/freetype
@@ -17,7 +30,7 @@ set libtiff=%SRC%/tiff
 set giflib=%SRC%/giflib
 set glut=%SRC%/glut
 
-set install=%ROOT%/3rdparty-bin/%Name%"
+set install=%ThirdPary_ROOT%publish/windows/%arch%
 
 set curl=%curl:\=/%
 set zlib=%zlib:\=/%
@@ -28,20 +41,25 @@ set libtiff=%libtiff:\=/%
 set giflib=%giflib:\=/%
 set glut=%glut:\=/%
 set install=%install:\=/%
+set build_dir=%build_dir:\=/%
+
+echo %install%
 
 
-
-  cmake  -G"Visual Studio 16 2019" -A x64 -B  .\build\osg-3rdparty  -D CURL_SOURCE_DIR=%curl% -D FREETYPE_SOURCE_DIR=%freetype% -D GIFLIB_SOURCE_DIR=%giflib% -D GLUT_SOURCE_DIR=%glut% -D LIBJPEG_SOURCE_DIR=%libjpeg% -D LIBPNG_SOURCE_DIR=%libpng% -D LIBTIFF_SOURCE_DIR=%libtiff% -D ZLIB_SOURCE_DIR=%zlib% -DCMAKE_INSTALL_PREFIX=%install%
+  cmake  -G"Visual Studio 16 2019" -A %arch% -B  %build_dir%  -D CURL_SOURCE_DIR=%curl% -D FREETYPE_SOURCE_DIR=%freetype% -D GIFLIB_SOURCE_DIR=%giflib% -D GLUT_SOURCE_DIR=%glut% -D LIBJPEG_SOURCE_DIR=%libjpeg% -D LIBPNG_SOURCE_DIR=%libpng% -D LIBTIFF_SOURCE_DIR=%libtiff% -D ZLIB_SOURCE_DIR=%zlib% -DCMAKE_INSTALL_PREFIX=%install%
 
 REM cmake  -D CURL_SOURCE_DIR=%curl% -D FREETYPE_SOURCE_DIR=freetype -D GIFLIB_SOURCE_DIR= %SRC%/giflib -D GLUT_SOURCE_DIR=%SRC%/glut -D LIBJPEG_SOURCE_DIR=%SRC%/libjpeg -D LIBPNG_SOURCE_DIR=%SRC%/libpng -D LIBTIFF_SOURCE_DIR=%SRC%/libtiff -D ZLIB_SOURCE_DIR= -P %ROOT%CMakeLists.txt
+echo %install%/Debug
+cmake --build %build_dir% --config "Debug" --target ALL_BUILD
+cmake -DBUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=%install%/Debug -P %build_dir%/cmake_install.cmake
 
-cmake --build ./build/osg-3rdparty  --config "Debug" --target install 
-    
-cmake --build ./build/osg-3rdparty  --config "Release" --target install 
-    
-cd %ROOT%3rdparty-bin\%Name%
-    
-7z a %ROOT%%Name%.zip 
+echo %install%/Release
+cmake --build %build_dir% --config "Release" --target ALL_BUILD
+cmake -DBUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%install%/Release -P %build_dir%/cmake_install.cmake
 
-cd %ROOT%
+REM echo %install%/RelWithDebInfo
+REM cmake --build %build_dir% --config "RelWithDebInfo" --target ALL_BUILD
+REM cmake -DBUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=%install%/RelWithDebInfo -P %build_dir%/cmake_install.cmake
+
+
 	
